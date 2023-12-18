4662a20c374a7561a254a222242da4fea30a019c
DELL <DELL@SEMIH>
2023-12-18 17:55:28 +0300

    advance logic with sql query insert

---
+++ AdvanceProject.API/Contract/Request/GetAdvanceApproveEmployeeRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceProject.API.Contract.Request
+{
+	public class GetAdvanceApproveEmployeeRequestModel
+	{
+		public int AdvanceId { get; set; }
+		public List<int> Titles { get; set; }
+	}
+}
---
+++ AdvanceProject.API/Contract/Request/GetAdvanceOrderConfirmRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceProject.API.Contract.Request
+{
+	public class GetAdvanceOrderConfirmRequestModel
+	{
+		public int BusinessUnitId { get; set; }
+		public List<int> Titles { get; set; }
+	}
+}

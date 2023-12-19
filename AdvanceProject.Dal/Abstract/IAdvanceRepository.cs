719da7f72fab5a88924f890a6c387a8d94f32fd9
DELL <DELL@SEMIH>
2023-12-19 17:55:49 +0300

    final

--- AdvanceProject.Dal/Abstract/IAdvanceRepository.cs
+++ AdvanceProject.Dal/Abstract/IAdvanceRepository.cs
@@ -8,18 +8,21 @@ namespace AdvanceProject.Dal.Abstract
 	public interface IAdvanceRepository
 	{
 		Task<Advance> AddAdvance(Advance advance);
+		//bekleyen avanslar
 		List<EmployeeAdvanceResponseDto> GetAdvanceListData(int employeeId);
+		//avansın detayları
 		List<AdvanceDetailDTO> GetAdvanceDetails(int advanceId);
 
 		List<AdvanceConfirmDTO> GetAdvanceConfirmEmployee(int employeeId);
 
 		List<int> GetTitleID(decimal advanceAmount);
 		List<AdvanceOrderConfirmDTO> GetAdvanceOrderConfirm(int businessUnitId, List<int> titles);
-		List<AdvanceApprovedEmployeeDTO> GetAdvanceApproveEmployee(int advanceID, List<int> titles);
+		List<AdvanceApprovedEmployeeDTO> GetAdvanceApproveEmployee(int advanceID, List<int> titles); //Onaylayacak çalışanlar
 
+		//Onayda kullanılan
 		Task<AdanceHistoryApproveDTO> AddAdvanceHistoryApprove(AdanceHistoryApproveDTO dto);
 		Task<bool> GetAdvanceChangeStatus(int advanceId, int nowStatus);
-
+		//Onay bekleyen avanslar page
 		Task<List<UserAdvanceListDTO>> GetUserAdvanceList(int employeeId, int businessUnitID);
 	}
 }
